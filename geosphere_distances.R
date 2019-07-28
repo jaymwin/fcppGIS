8fd709573d2fef02868e49a683c8f0be7979e428
Jay Winiarski <jasonwiniarski@u.boisestate.edu>
2019-07-28 14:33:09 -0600

    Add files via upload

---
+++ geosphere_distances.R
@@ -0,0 +1,24 @@
+
+library(readxl)
+library(geosphere)
+
+dat <- read_excel(here('raw_data/all_kestrel_returns.xlsx'))
+dat
+
+dat <- dat %>%
+  select(tmin, Bflyway, winter, sex, Blat, Blong, Elat, Elong, dist)
+dat
+
+# function to calc. distances
+calculate_distances <- as_mapper(~distm(x = c(..6, ..5),
+                                        y = c(..8, ..7),
+                                        fun = distHaversine) / 1000)
+# calculate with pmap_dbl
+dat <- dat %>%
+  mutate(
+    distance = pmap_dbl(., calculate_distances)
+  )
+dat
+
+ggplot(dat, aes(dist, distance)) +
+  geom_point()
